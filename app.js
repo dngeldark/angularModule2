f6f31450dbdfe5de64b393c12f08b1fa059b4888
Manuel Hernandez <dngeldark@gmail.com>
2016-09-26 13:06:16 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,72 @@
+(function(){
+	'use strict';
+
+	angular.module('shoppingApp',[])
+		.controller('ToBuyController',['ShoppingListCheckOffService', function(ShoppingListCheckOffService){
+			this.list = ShoppingListCheckOffService.getItems();
+
+			this.checkList = function(){
+				return !ShoppingListCheckOffService.listStatus();
+			}
+			this.removeFromList = function(item){
+				item.buy = false;
+				ShoppingListCheckOffService.prestine = false;
+			}
+
+		}])
+
+		.controller('AlreadyBoughtController',['ShoppingListCheckOffService', function(ShoppingListCheckOffService){
+			this.checkList = function(){
+				return ShoppingListCheckOffService.prestine;
+			}
+			this.list = ShoppingListCheckOffService.getItems();
+		}])
+
+		.service('ShoppingListCheckOffService', function(){
+			var service = this;
+
+			service.prestine = true;
+
+			var itemsList = [
+			{
+				name: "cookies",
+				quantity: "10 bags",
+				buy: true
+			},
+			{
+				name: "tamales",
+				quantity: "2 packs",
+				buy: true
+			},
+			{
+				name: "popcorn",
+				quantity: "1 bag",
+				buy: true
+			},
+			{
+				name: "sodas",
+				quantity: "5 bottles",
+				buy: true
+			},
+			{
+				name: "chips",
+				quantity: "3 bags",
+				buy: true
+			}
+			];
+
+			service.getItems = function(){
+				return itemsList;
+			}
+
+			service.listStatus = function(){
+				for(var i=0;i<itemsList.length;i++){
+					if(itemsList[i].buy == true) return false;
+				}
+				return true;
+			}
+
+
+		});
+
+})()
